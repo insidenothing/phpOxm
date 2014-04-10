fa31c916fe0ea98b55572f3285bc497989d21ea3
Patrick McGuire <insidenothing@gmail.com>
2014-04-10 10:49:24 -0400

    looking good

--- monitor.php
+++ monitor.php
@@ -3,26 +3,17 @@
 ob_start();
 $last_line = system('xm list', $retval);
 $buffer = ob_get_clean();
-
-
 function checkNstart($vm,$buffer){
-
-  $pos = strpos($buffer, $vm);
-  if ($pos === false) {
-      echo "'$vm' DOWN
-      ";
-      
-  } else {
-      echo "'$vm' UP
-      ";
-      // do nothing =)
-  }
-
+$pos = strpos($buffer, $vm);
+if ($pos === false) {
+echo "'$vm' DOWN
+";
+} else {
+echo "'$vm' UP
+";
+// do nothing =)
 }
-
-
-
-
+}
 checkNstart('qb2013',$buffer);
 checkNstart('fileserver2 ',$buffer);
 checkNstart('fileserver ',$buffer);
