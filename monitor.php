7bf96ae139ea3a5d5e66ce3d77d27be93ec98999
Patrick McGuire <insidenothing@gmail.com>
2014-04-10 10:54:19 -0400

    include

--- monitor.php
+++ monitor.php
@@ -1,19 +1,9 @@
 <?PHP
 // this file will check for the existance of a runing VM, if it is not running, start it
+include 'functions.php';
 ob_start();
 $last_line = system('xm list', $retval);
 $buffer = ob_get_clean();
-function checkNstart($vm,$buffer){
-$pos = strpos($buffer, $vm);
-if ($pos === false) {
-echo "'$vm' DOWN
-";
-} else {
-echo "'$vm' UP
-";
-// do nothing =)
-}
-}
 checkNstart('qb2013',$buffer);
 checkNstart('fileserver2 ',$buffer);
 checkNstart('fileserver ',$buffer);
