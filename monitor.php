f6a02bd939b70d28e6eb6d56ab40b86cb69e99c4
Patrick McGuire <insidenothing@gmail.com>
2014-04-10 12:00:54 -0400

    newline

--- monitor.php
+++ monitor.php
@@ -1,5 +1,5 @@
 <?PHP
-error_log(date('r').": Begin Monitor", 3, 'php.log');
+error_log(date('r').": Begin Monitor \n", 3, 'php.log');
 // this file will check for the existance of a runing VM, if it is not running, start it
 include 'functions.php';
 ob_start();
@@ -8,5 +8,5 @@ $buffer = ob_get_clean();
 checkVM('qb2013',$buffer);
 checkVM('fileserver2 ',$buffer);
 checkVM('fileserver ',$buffer);
-error_log(date('r').": End Monitor", 3, 'php.log');
+error_log(date('r').": End Monitor \n", 3, 'php.log');
 ?>
